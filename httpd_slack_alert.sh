3a8ce4f5b46e4c291da5a18dc9ee3dcb54fbe62a
D-Gurukishore Bolisetty <d-boliseg@U-16XTRFGVJW9XA.dev.dacc.phz>
2025-10-01 11:59:14 +0100

    updated cronfile with slack aler script

---
+++ httpd_slack_alert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Fetch the Slack webhook securely from SSM Parameter Store
+SLACK_WEBHOOK=$(aws ssm get-parameter \
+  --name "slack_notification_webhook" \
+  --with-decryption \
+  --region eu-west-2 \
+  --query "Parameter.Value" \
+  --output text)
+
+# Check if the httpd process is running
+if ! pgrep -x "httpd" > /dev/null; then
+  HOSTNAME=$(hostname)
+  MESSAGE="[$HOSTNAME] ALERT: httpd process is not running!"
+
+  # Send Slack alert
+  curl -X POST -H 'Content-type: application/json' --data "{
+    \"text\": \"$MESSAGE\"
+  }" "$SLACK_WEBHOOK"
+fi
